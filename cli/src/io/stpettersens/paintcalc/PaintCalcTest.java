e4512d2e354a48fbbd6daa97fe5d571b0c37c2d7
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-06-27 22:47:53 +0100

    +start writing unittests.

---
+++ cli/src/io/stpettersens/paintcalc/PaintCalcTest.java
@@ -0,0 +1,18 @@
+/*
+    Utility to calculate the volume of paint needed to
+    paint a room of supplied dimensions.
+
+    Sam Saint-Pettersen, 2017.
+*/
+
+package io.stpettersens.paintcalc;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PaintCalcTest {
+
+    @Test
+    public void testCalculatePaintNeeded() {
+        
+    }
+}
